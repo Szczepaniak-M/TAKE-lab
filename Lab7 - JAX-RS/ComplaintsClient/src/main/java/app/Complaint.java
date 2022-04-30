d198ef22bed3b93e0ba68acc28fef3b955a217b4
Szczepaniak-M <m.szczepaniak99@gmail.com>
2022-04-30 11:57:11 +0200

    Lab 7

---
+++ Lab7 - JAX-RS/ComplaintsClient/src/main/java/app/Complaint.java
@@ -0,0 +1,86 @@
+package app;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import java.time.LocalDate;
+
+public class Complaint {
+
+    private Long id;
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate complaintDate;
+    private String complaintText;
+    private String author;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getComplaintDate() {
+        return complaintDate;
+    }
+
+    public void setComplaintDate(LocalDate complaintDate) {
+        this.complaintDate = complaintDate;
+    }
+
+    public String getComplaintText() {
+        return complaintText;
+    }
+
+    public void setComplaintText(String complaintText) {
+        this.complaintText = complaintText;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Complaint)) {
+            return false;
+        }
+        Complaint other = (Complaint) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Complaint[ id=%s, complaintDate=%s, complaintText=%s, author=%s, status=%s]%n",
+                id, complaintDate, complaintText, author, status);
+    }
+}
---
+++ Lab7 - JAX-RS/ComplaintsClient/src/main/java/app/Main.java
@@ -0,0 +1,60 @@
+package app;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.util.Arrays;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.glassfish.jersey.client.ClientConfig;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ClientConfig config = new ClientConfig();
+        config.register(new ObjectMapper().registerModule(new JavaTimeModule()));
+        Client client = ClientBuilder.newClient(config);
+
+        String count = client.target("http://localhost:8080/Complaints/"
+                + "resources/complaints/count")
+                .request(MediaType.TEXT_PLAIN)
+                .get(String.class);
+        System.out.println("Count: " + count);
+
+        Complaint[] complaints = client.target("http://localhost:8080/Complaints/"
+                + "resources/complaints")
+                .request(MediaType.APPLICATION_JSON)
+                .get(Complaint[].class);
+        System.out.println("All complaints before update: \n" + Arrays.toString(complaints));
+
+        Complaint complaint = null;
+        for (Complaint com : complaints) {
+            if (com.getStatus().equals("open")) {
+                complaint = client.target("http://localhost:8080/Complaints/"
+                        + "resources/complaints/" + com.getId())
+                        .request(MediaType.APPLICATION_JSON)
+                        .get(Complaint.class);
+                break;
+            }
+        }
+        System.out.println("Open complaint:\n" + complaint);
+        
+        complaint.setStatus("closed");
+        Response response = client.target("http://localhost:8080/Complaints/"
+                + "resources/complaints/" + complaint.getId())
+                .request()
+                .put(Entity.entity(complaint, MediaType.APPLICATION_JSON));
+        System.out.println("Status:\n"+ response.getStatus());
+        
+        Complaint[] openComplaints = client.target("http://localhost:8080/Complaints/"
+                + "resources/complaints")
+                .queryParam("status", "open")
+                .request(MediaType.APPLICATION_JSON)
+                .get(Complaint[].class);
+        System.out.println("Open complaints:\n" + Arrays.toString(openComplaints));
+
+        client.close();
+    }
+}
